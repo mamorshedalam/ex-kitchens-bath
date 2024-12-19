f5764a2680d126fbf918446f5468368c6eb3c4ac
mamorshedalam <ma.morshedalampro@gmail.com>
2024-12-19 19:01:57 +0800

    first desing complete of front page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { MoveRight } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
@@ -59,6 +60,20 @@ export default function Home() {
       link: "",
     },
   ];
+  const brandArray = [
+    "/brand/brand-1.svg",
+    "/brand/brand-2.svg",
+    "/brand/brand-3.svg",
+    "/brand/brand-4.svg",
+    "/brand/brand-5.svg",
+    "/brand/brand-7.svg",
+    "/brand/brand-8.png",
+    "/brand/brand-9.svg",
+    "/brand/brand-10.svg",
+    "/brand/brand-11.svg",
+    "/brand/brand-12.svg",
+    "/brand/brand-13.svg",
+  ];
   return (
     <>
       <section className="section-container">
@@ -80,6 +95,37 @@ export default function Home() {
                     See more
                   </Link>
                 </div>
+              </div>
+            ))}
+        </div>
+      </section>
+
+      <section className="section-container !py-0">
+        <div className="section-banner bg-[url('/banner.jpg')]">
+          <div className="relative z-10">
+            <h1 className="font-semibold lg:text-5xl md:text-4xl text-2xl tracking-wider lg:mb-4 md:mb-2">
+              Finance Your Home Project
+            </h1>
+            <Link href={`/`}>
+              <button className="w-full flex items-center md:justify-start justify-center gap-2 md:text-xl text-lg hover:underline">
+                <span>Explore your financing options</span>
+                <MoveRight size={21} />
+              </button>
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section className="section-container">
+        <h2 className="section-title">Featured brands</h2>
+        <div className="flex flex-wrap items-center justify-center">
+          {brandArray &&
+            brandArray.map((link, index) => (
+              <div
+                key={index}
+                className="max-w-60 lg:w-1/6 w-1/4 md:px-6 px-2.5"
+              >
+                <img src={link} alt="Brand" className="w-full" />
               </div>
             ))}
         </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+import Logo from "./Logo";
+import { Mail, Phone } from "lucide-react";
+
 export default function Footer() {
   return (
-    <>
-      <p>footer</p>
-    </>
+    <footer className="relative max-w-screen-2xl text-center md:p-9 p-4 mx-auto bg-gray-100">
+      <Logo />
+      <div className="flex items-center justify-center md:gap-2 md:mt-4 mt-2">
+        <Link href={`/`}>
+          <button className="btn-icon">
+            <Mail size={21} />
+          </button>
+        </Link>
+        <Link href={`/`}>
+          <button className="btn-icon">
+            <Phone size={21} />
+          </button>
+        </Link>
+      </div>
+      <p className="font-medium">36 Chestnut Rd, Paoli, PA 19301</p>
+    </footer>
   );
 }
